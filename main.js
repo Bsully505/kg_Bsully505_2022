b9fd99861f8e6e57f6829d17a5b03a2a039b16a9
Bsully505 <bsully505@yahoo.com>
2021-05-02 23:11:35 -0400

    Created an index based array which has the phonetic equivalent based of the index and

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ Application for Kargo
 
 
 let Values = process.argv.slice(2);//assigning an array for these argument values
-console.log(Values);//This is to show all of the arguments passed on to the class
-
+let phoneticEquivalent = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];//This is used as a key for being able to get the phonetic equivalent
 let NumOfString = [];//this is going to be the return value
 for(x in Values){//iterate through all of the arguments
 let str = "";//base string which will be used to add all phonetic equivalent values in a individual argument
 for(y in Values[x]){// for loop to traverse throughout each digit place
-console.log((Values[x])[y])//Views the current digit in respect to the digit place
+str+= phoneticEquivalent[parseInt((Values[x])[y])]
 }
-console.log();//used to separate each integer value from the argument
+NumOfString.push(str)//allows to append the phonetic equivalent
 }
+console.log(NumOfString.toString())//this prints out the result of the argument to their word equivalent
